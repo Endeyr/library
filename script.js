d510f330dff969f2fcdc1299b0f60d0598fa124c
Endeyr <aaron.thompson42594@gmail.com>
2022-08-16 11:24:50 -0400

    add javascript for form button

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 let myLibrary = []
+let newBook
+let tempLibrary
 
-function Book() {
-	// the constructor...
+function Book(authorFirstName, authorLastName, title, pages, progress) {
+	this.authorFirstName = authorFirstName
+	this.authorLastName = authorLastName
+	this.title = title
+	this.pages = pages
+	this.progress = progress
+	this.info = function () {
+		return (
+			title +
+			' by' +
+			authorFirstName +
+			' ' +
+			authorLastName +
+			', ' +
+			pages +
+			' pages' +
+			', read:' +
+			progress
+		)
+	}
 }
 
 function addBookToLibrary() {
 	// do stuff here
 }
+
+const showFormBtn = document.getElementById('showFormBtn'),
+	form = document.getElementById('form')
+showFormBtn.addEventListener('click', () => {
+	showFormBtn.classList.remove('d-block')
+	showFormBtn.classList.add('d-none')
+	form.classList.remove('d-none')
+	form.classList.add('d-block')
+})
